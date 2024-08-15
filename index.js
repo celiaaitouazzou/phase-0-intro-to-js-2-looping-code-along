816326c13431bfa9170efb2bf0fb4e99d83f3fba
Celia Ait Ouazzou <celiaaitouazzou@gmail.com>
2024-08-15 18:23:05 -0400

    new file added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(nameList,occasion)
+{
+    let cards = [];
+
+    for(let i = 0 ; i < nameList.length ; i++)
+    {
+        cards.push(`Thank you, ${nameList[i]}, for the wonderful ${occasion} gift!`)
+    }
+
+    return cards;
+}
+
+function countDown(n)
+{
+    for(let i = n; i >= 0;i--){
+        console.log(i);
+    }
+}
